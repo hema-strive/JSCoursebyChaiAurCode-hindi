383a6d9559f7946c98852c61cb3dff85d762e24b
hema <62857891+hema-strive@users.noreply.github.com>
2025-12-29 23:19:13 +0000

    variables in JS

---
+++ basics_01/01_variable.js
@@ -0,0 +1,26 @@
+const accountId = 12345
+let accountName ="Hema"
+var accountCity = "Muz"
+accountPassword = "4567"
+let accountState
+
+//basics_01/01_variable.jsaccountId = 67
+//console.log(accountId);
+accountName = "Kumar"
+console.log(accountName);
+accountCity = "Noida"
+console.log(accountCity);
+
+accountPassword = "9087"
+console.log(accountPassword);
+console.table([accountState])
+
+/*
+prefer not to use var
+because of issue in block and functional scope
+
+{} is known as scope
+
+*/
+
+
